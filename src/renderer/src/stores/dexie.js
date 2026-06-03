5ff56012ae15cca9ccbdd7ccc6181bc339d21a05
Ahonui <116345502+AhonuiHawaii@users.noreply.github.com>
2026-06-03 12:07:20 -1000

    refactor: update database schema by removing categories and correcting database name

--- src/renderer/src/stores/dexie.js
+++ src/renderer/src/stores/dexie.js
@@ -1,11 +1,9 @@
 import Dexie from 'dexie'
-import defaultCategories from '../assets/categories.json'
 
 // DO NOT add db.version() migrations. Bump the version number only if the schema must change.
-const db = new Dexie('BudgetAppFrontendDB')
+const db = new Dexie('Budgets')
 
 db.version(1).stores({
-  categories: 'id, name, type, createdAt',
   budgets: 'id, type, amount, month, createdAt',
   debtDetails: 'id, updatedAt'
 })
